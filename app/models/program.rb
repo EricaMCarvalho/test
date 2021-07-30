866d006b259f5142df6a8301065c5d7f3c1689eb
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-30 14:43:44 +0100

    fix rails validation error messages

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,6 @@
 class Program < ApplicationRecord
   validates :name, presence: true, length: { in: 3..50 }
   validates :lockSubmitted, :multipleSubmissions, :makeAnonymous, :scoreSharing, inclusion: [true, false]
-  validates :confirmationText, presence: true, length: { minimum: 8 }
-  validates :redirectUrl, :logoutUrl, allow_blank: true, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z/ix, message: 'Please add a valid url' }
+  validates :confirmationText, presence: true, length: { minimum: 8, too_short: 'must be at least 8 characters long. ' }
+  validates :redirectUrl, :logoutUrl, allow_blank: true, format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(\/.*)?\z/ix, message: 'is not valid. ' }
 end
