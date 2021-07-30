a3630fccd2f49370d6c0d8d3813684f44f7c9649
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-30 02:56:18 +0100

    add get program endpoint

--- app/controllers/api/programs_controller.rb
+++ app/controllers/api/programs_controller.rb
@@ -4,6 +4,11 @@ module Api
       @programs = Program.all
       render json: @programs
     end
+
+    def show
+      @program = Program.find(params[:id])
+      render json: @program
+    end
   end
 end
 
