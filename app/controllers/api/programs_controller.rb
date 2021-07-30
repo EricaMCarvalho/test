32b31e62d151cbb53d53299c1d44129d863af359
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-30 03:13:02 +0100

    add update endpoint

--- app/controllers/api/programs_controller.rb
+++ app/controllers/api/programs_controller.rb
@@ -9,7 +9,14 @@ module Api
       @program = Program.find(params[:id])
       render json: @program
     end
+
+    def update
+      @program = Program.find(params[:id])
+      if @program.update(params.require(:program).permit(:name, :lockSubmitted, :multipleSubmissions, :confirmationText, :redirectUrl, :logoutUrl, :makeAnonymous, :scoreSharing))
+        render json: @program
+      else
+        render json: {"error": "Failed to save changes"}
+      end
+    end
   end
 end
-
-
